e02340a6510dd6c2905774187c0fe03eac503a7b
Sanjay <sunnymulchandani07@gmail.com>
2022-06-14 15:30:10 +0530

    updated project

--- login/forms.py
+++
@@ -1,6 +0,0 @@
-import imp
-from django import forms
-
-class loginForm(forms.Form):
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(max_length=50, widget=forms.PasswordInput)
