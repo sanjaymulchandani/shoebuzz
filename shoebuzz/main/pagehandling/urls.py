8c8a992b6b5930ca5a379f38d3d5f9434c1a019e
Sanjay <sunnymulchandani07@gmail.com>
2022-06-16 17:25:21 +0530

    urls updated for all pages

--- shoebuzz/main/pagehandling/urls.py
+++ shoebuzz/main/pagehandling/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('',views.landingPage,name='landingPage'),
     path('bags',views.bagsPage,name='bagsPage'),
+    path('clothing',views.clothingPage,name='clothingPage'),
+    path('sale',views.salePage,name='salePage'),
+    path('shoes',views.shoesPage,name='shoesPage'),
+    path('sports',views.sportsPage,name='sportsPage'),
+    path('support',views.supportPage,name='supportPage'),
+    path('login',views.loginPage,name='loginPage'),
+    path('register',views.registerPage,name='registerPage'),    
 ]
--- shoebuzz/main/pagehandling/views.py
+++ shoebuzz/main/pagehandling/views.py
@@ -5,3 +5,17 @@ def landingPage(request):
     return render(request,'index.html')
 def bagsPage(request):
     return render(request,'bags.html')
+def clothingPage(request):
+    return render(request,'clothing.html')
+def shoesPage(request):
+    return render(request,'shoes.html')
+def sportsPage(request):
+    return render(request,'sports.html')
+def salePage(request):
+    return render(request,'sale.html')
+def supportPage(request):
+    return render(request,'support.html')
+def loginPage(request):
+    return render(request,'login.html')
+def registerPage(request):
+    return render(request,'register.html')
